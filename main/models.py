6323c27d1889a880e2dd270097bafb244a8c41b2
Vannen <vannenc@gmail.com>
2013-01-24 03:43:47 +0400

    Slug generator modified

--- main/models.py
+++ main/models.py
@@ -1,8 +1,36 @@
 from django.db import models
+import random
+
 
 class TinyUrl(models.Model):
     url = models.URLField(verify_exists=False, null=False)
     tiny = models.CharField(null=False, max_length=50)
 
+    def save(self, *args, **kwargs):
+
+        self.tiny = self.generate_slug()
+
+        try:
+            found = TinyUrl.objects.filter(tinyurl=slug).count()
+
+            while found != 0:
+                self.tiny = self.generate_slug()
+                found = TinyUrl.objects.filter(tinyurl=slug).count()
+
+        except Exception, e:
+            pass
+
+        super(TinyUrl, self).save(*args, **kwargs) # Call the "real" save() method.
+
     def __unicode__(self):
         return "%s - %s" % (str(self.url), self.tiny)
+
+    def generate_slug(self):
+
+        h = [chr(random.randrange(65, 91)), chr(random.randrange(97, 123)),
+        random.randrange(0, 10), chr(random.randrange(97, 123)),
+        chr(random.randrange(97, 123))]
+
+        random.shuffle(h)
+
+        return ''.join(str(i) for i in h)
--- main/views.py
+++ main/views.py
@@ -5,28 +5,24 @@ from django.views.generic import TemplateView
 from django.views.generic import RedirectView
 from main.models import TinyUrl
 
+
 class IndexView(TemplateView):
     """ index page """
-    template_name='home/index.html'
+    template_name = 'home/index.html'
 
 
 class TinyUrlRedirectView(RedirectView):
     """ """
 
     def get_redirect_url(self, **kwargs):
-        if kwargs['tinyurl'] is not None:
+        """ redirect part"""
+        try:
 
-            try:
+            tinyurl = TinyUrl.objects.get(tiny__exact=kwargs['tinyurl'])
+            return tinyurl.url
 
-                tinyurl = TinyUrl.objects.get(tiny=kwargs['tinyurl'])
-                return tinyurl.url
-
-            except Exception, e:
-                raise Http404
-
-        else:
-            return None
-
+        except Exception, e:
+            raise Http404
 
 
 def index(request):
