5f0ec79694c429a9da883333f302c936923b8c07
Vannen <vannenc@gmail.com>
2013-01-26 19:55:19 +0400

    JS disabled version working now

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 #! /usr/local/env python
-from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.http import Http404
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 from django.views.generic import TemplateView
 from django.views.generic import RedirectView
 from main.models import TinyUrl
@@ -25,13 +26,13 @@ class TinyUrlRedirectView(RedirectView):
             tinyurl = TinyUrl.objects.get(tiny__exact=kwargs['tinyurl'])
             return tinyurl.url
 
-        except Exception, e:
+        except Exception:
             raise Http404
 
 
 def create_tinyurl(request):
     """ """
-    create_form = TinyUrlForm(request.GET)
+    create_form = TinyUrlForm(request.POST)
     output = {'url': '', 'error': '', 'tiny': '', 'status': ''}
 
     if create_form.is_valid() is True:
@@ -45,13 +46,9 @@ def create_tinyurl(request):
         output['error'] = create_form.errors['url'][0]
         output['status'] = 'NOK'  # not ok
 
-    if request.GET.get('format', None) is not None:
+    if request.POST.get('format', None) is not None:
 
-        if request.GET.get('callback', None) is None:
-            return HttpResponse(json.dumps(output))
-
-        else:
-            return HttpResponse("%s(%s)" % (request.GET.get('callback'), json.dumps(output)))
+        return HttpResponse(json.dumps(output))
 
     else:
-        return HttpResponse(json.dumps(output))
+        return render_to_response('home/index.html', {'output': output}, context_instance=RequestContext(request))
---
+++ urlshortener/static/scripts/main.js
@@ -0,0 +1,61 @@
+jQuery(function(){
+
+    $('form#form-create').on('submit', function(e){
+
+        var url = $('input#url-text');
+        var errorsContainer = $('div#errors');
+        var successContainer = $('div#success');
+        var overlay = $('#overlay');
+        var formurl = $(this).attr('action');
+
+        if(url.val().length == 0){
+            errorsContainer.append('<p>Please enter a url.<p>');
+            errorsContainer.fadeIn(500);
+
+            e.preventDefault();
+            return false;
+        }
+
+        errorsContainer.fadeOut(100);
+        overlay.fadeIn(300);
+
+        $.ajax({
+            'url' : formurl,
+            'type' : 'post',
+            'dataType' : 'json',
+            'data' : $.param($('form#form-create input')), //{'url' : url.val(), 'format' : 'js'},
+
+            'error': function(xhr, textstatus, error){
+                errorsContainer.html('');
+                overlay.fadeOut(100);
+                errorsContainer.append('<p>An error has occured. Please try again.<p>');
+                errorsContainer.fadeIn(500);
+            },
+
+            'success': function(data,textStatus,jqXHR){
+
+
+                overlay.fadeOut(100);
+
+                if(data.error != ''){
+                    successContainer.hide();
+                    
+                    errorsContainer.html('');
+                    overlay.fadeOut(100);
+                    errorsContainer.append('<p>' + data.error +'<p>');
+                    errorsContainer.fadeIn(500);
+
+                }else{
+                    successContainer.find('input').val(data.tiny);
+                    successContainer.fadeIn(300);
+                }
+
+            }
+
+
+        });
+
+        e.preventDefault();
+        return false;
+    });
+});
